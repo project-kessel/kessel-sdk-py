255e157d465c6f42f5b9da74547b014a937d127a
Paul Wayper <paulway@redhat.com>
2025-09-25 09:36:37 +1000

    Correct return types and while loop formation
    Signed-off-by: Paul Wayper <paulway@redhat.com>

--- src/kessel/inventory/get_resources.py
+++ src/kessel/inventory/get_resources.py
@@ -2,6 +2,7 @@ from kessel.inventory.v1beta2 import (
     # ClientBuilder,
     inventory_service_pb2_grpc,
     representation_type_pb2,
+    resource_reference_pb2,
     subject_reference_pb2,
     streamed_list_objects_request_pb2,
 )
@@ -14,7 +15,9 @@ def get_resources(
     subject: subject_reference_pb2.SubjectReference,
     limit: int = 20,
     fetch_all=True
-):
+) -> Generator[
+    resource_reference_pb2.ResourceReference, None, None
+]:
     """
     Get a continuous stream of the object type this subject has this relation
     to.  Works around the inherent pagination and continuation token handling.
@@ -31,11 +34,9 @@ def get_resources(
 
     """
     continuation_token = None
-    while (
-        response := _get_resource_page(
-            object_type, relation, subject, limit, continuation_token
-        ) is not None:
-    ):
+    while (response := _get_resource_page(
+        object_type, relation, subject, limit, continuation_token
+    )) is not None:
         for data in response:
             yield data.object
         if not fetch_all:
@@ -56,7 +57,7 @@ def _get_resource_page(
     subject: subject_reference_pb2.SubjectReference,
     limit: int,
     continuation_token: Optional[str] = None
-):
+) -> streamed_list_objects_response_pb2.StreamedListObjectsResponse:
     """
     Get a single page, of at most limit size, from continuation_token (or
     start if None).
